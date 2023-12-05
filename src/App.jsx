89fbd67ca99bb04ed2740ed582eaa1ec97dd1e03
aw3s0m3ss <aw3s0m3ss@gmail.com>
2023-12-05 19:39:37 +0200

    added count function  - +

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Catalog from "./Components/Catalog.";
 import Navbar from "./Components/Navbar";
 import ClickDemo from "./Components/demo/clickDemo";
 import StateDemo from "./Components/demo/StateDemo";
+import { useState } from "react";
 function App() {
   // const style = {
   //   fontSize: "300px",
@@ -22,6 +23,7 @@ function App() {
       <div className="catalog">
      <Catalog/>
     </div>
+    <div className="hibur"><Hibur/></div>
     <div className="button">
 
      <ClickDemo/>
@@ -40,3 +42,24 @@ function App() {
 
 
 export default App
+
+
+
+
+     function Hibur(){
+      const [number,setNumber] = useState(0);
+
+      function addtoNumber(){
+        setNumber(number+1)
+      
+      }
+      function takefromNumber(){
+        setNumber(number - 1)
+      }
+
+      return <div>
+        <button onClick={addtoNumber}>+</button>
+        {number}
+        <button onClick={takefromNumber}>-</button>
+      </div>
+     }
